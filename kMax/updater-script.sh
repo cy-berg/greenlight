934beb42943774a8131593783b4ad2f74b4ef803
cy-berg <93953580+cy-berg@users.noreply.github.com>
2024-05-29 21:49:43 +0200

    Update updater-script.sh

--- kMax/updater-script.sh
+++ kMax/updater-script.sh
@@ -4,10 +4,9 @@ Green='\033[0;32m'
 Cyan='\033[0;36m'
 NC='\033[0m' # No Color
 
-echo -e "${Cyan}delete the old files ${NC}"
-rm de.json en.json fa.json de.yml en.yml fa.yml application.html.erb
+echo -e "${Cyan}delete the Directory-files,json,yml etc. ${NC}"
+rm de.json en.json fa.json de.yml en.yml fa.yml docker-compose.yml application.html.erb
 #
-echo -e"${Green}downloading new file from github ${NC}"
 echo -e"${Green} docker compose down ${NC}"
 docker compose down
 echo -e "${Green} downloading the json, yml and erb Files from Github ${NC}"
@@ -19,6 +18,6 @@ wget https://raw.githubusercontent.com/cy-berg/greenlight/main/kMax/en.yml
 wget https://raw.githubusercontent.com/cy-berg/greenlight/main/kMax/fa.yml
 wget https://raw.githubusercontent.com/cy-berg/greenlight/main/kMax/application.html.erb
 #
-echo -e "${Green} docker compose up -d  runs ${NC}"
+echo -e "${Green} docker compose up -d  runs /Building Containers ${NC}"
 #
 docker compose up -d
