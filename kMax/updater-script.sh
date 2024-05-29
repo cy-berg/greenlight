a7dfbafec419de78186f01d95ffd57d83f3d2602
cy-berg <93953580+cy-berg@users.noreply.github.com>
2024-05-29 22:41:44 +0200

    Update updater-script.sh

--- kMax/updater-script.sh
+++ kMax/updater-script.sh
@@ -4,11 +4,11 @@ Cyan='\033[0;36m'
 purple='\034[0;35m'
 NoColor='\033[0m' # No Color
 
-echo -e "${Green} docker compose goes down and all of images will be deleted ${NoColor}"
+echo -e "${purple} docker compose down AND all of images will be deleted ${NoColor}"
 #docker compose -f docker-compose.yml down
 docker compose down --rmi all
 echo "did it worked___???"
-echo -e "${purple}delete the Directory-files,json,yml etc. ${NoColor}"
+echo -e "${Green}delete the Directory-files,json,yml etc. ${NoColor}"
 rm de.json en.json fa.json de.yml en.yml fa.yml docker-compose.yml application.html.erb
 #
 echo -e "${Green} downloading the json, yml and erb Files from Github ${NoColor}"
@@ -21,6 +21,6 @@ wget https://raw.githubusercontent.com/cy-berg/greenlight/main/kMax/en.yml
 wget https://raw.githubusercontent.com/cy-berg/greenlight/main/kMax/fa.yml
 wget https://raw.githubusercontent.com/cy-berg/greenlight/main/kMax/application.html.erb
 #
-echo -e "${Green} docker compose up -d  runs /Building Containers ${NoColor}"
+echo -e "${Green} docker compose up -d  runs /must rebuild Containers ${NoColor}"
 #
 docker compose up -d
