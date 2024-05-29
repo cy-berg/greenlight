2db910492d3163c10ea8b4a5897fc5ad879261d1
cy-berg <93953580+cy-berg@users.noreply.github.com>
2024-05-29 22:54:17 +0200

    Update updater-script.sh

--- kMax/updater-script.sh
+++ kMax/updater-script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 Green='\033[0;32m' 
 Cyan='\033[0;36m'
-purple='\034[0;35m'
+Magenta='\u001b[35m'
 NoColor='\033[0m' # No Color
 
-echo -e "${purple} docker compose down AND all of images will be deleted ${NoColor}"
+echo -e "${Magenta} docker compose down AND all of images will be deleted ${NoColor}"
 #docker compose -f docker-compose.yml down
 docker compose down --rmi all --volumes
 echo "did it worked___???"
@@ -21,6 +21,6 @@ wget https://raw.githubusercontent.com/cy-berg/greenlight/main/kMax/en.yml
 wget https://raw.githubusercontent.com/cy-berg/greenlight/main/kMax/fa.yml
 wget https://raw.githubusercontent.com/cy-berg/greenlight/main/kMax/application.html.erb
 #
-echo -e "${Green} docker compose up -d  runs /must rebuild Containers ${NoColor}"
+echo -e "${Cyan} docker compose up -d  runs /must rebuild Containers ${NoColor}"
 #
 docker compose up -d
