7eb02c70644765c278b0ac8f1be6412b0c669f4b
cy-berg <93953580+cy-berg@users.noreply.github.com>
2024-05-30 09:36:42 +0200

    Update-v2.1 updater-script.sh

--- kMax/updater-script.sh
+++ kMax/updater-script.sh
@@ -4,13 +4,13 @@ Cyan='\033[0;36m'
 Magenta='\u001b[35m'
 NoColor='\033[0m' # No Color
 
-echo -e "${Magenta} docker compose down AND all of images will be deleted ${NoColor}"
-#docker compose -f docker-compose.yml down
+#echo -e "${Magenta} docker compose down AND all of Containers will be deleted ${NoColor}"
 #docker compose down --rmi all
+echo -e "${Magenta} docker compose down AND all of Containers will be deleted ${NoColor}"
 docker compose down
 echo "did it delete all Containers???"
-echo -e "${Cyan} please wait: it starts in a few secound. ${NoColor}"
-sleep 10s
+echo -e "${Cyan} please wait: it starts in a few secounds. ${NoColor}"
+sleep 5s
 echo -e "${Green}delete the Directory-files,json,yml etc. ${NoColor}"
 rm de.json en.json fa.json de.yml en.yml fa.yml docker-compose.yml application.html.erb
 #
